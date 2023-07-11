17360fd2f6b8e1851e38ee67ec9b743688a4c829
gkudemus <gene.kudemus@gmail.com>
2023-07-11 09:58:59 +0800

    add breed label

--- my-dogs-app/src/components/FavoritesCard.jsx
+++ my-dogs-app/src/components/FavoritesCard.jsx
@@ -16,7 +16,7 @@ const FavoritesCard = ({image}) => {
     <div className="max-w-sm rounded overflow-hidden shadow-lg p-3 md:p-0">
       <div className='flex flex-col'>
         <img src={image} className='md:w-56 md:h-56' onClick={() => removeFormLikePage(image)}/>
-        <p>{breedName}</p>
+        <p><b>Breed: </b>{breedName}</p>
       </div>
       <div className='flex flex-col'>
         <button onClick={() => removeFormLikePage(image)} className="my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
